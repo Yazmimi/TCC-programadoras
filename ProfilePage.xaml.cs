af2868ba5a66563ed5607b995134511821d371fd
Yazmimi <yasmim.pereisantos.08@gmail.com>
2025-04-08 17:08:06 -0300

    08/04Final

--- ProfilePage.xaml.cs
+++ ProfilePage.xaml.cs
@@ -8,12 +8,41 @@ namespace TCC
 {
     public partial class ProfilePage : Page
     {
-        
+        private string connectionString = "Server=localhost;Database=Cadastros;Uid=root;Pwd=;";
 
         public ProfilePage()
         {
             InitializeComponent();
-           
+            CarregarDadosPerfil();
+        }
+
+        private void CarregarDadosPerfil()
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT usuario, telefone, regiao, email FROM usuariosCadastrados WHERE cod = @UsuarioId";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@UsuarioId", SessaoUsuario.UsuarioId);
+
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            txtNome.Text = reader["usuario"].ToString();
+                            txtTelefone.Text = reader["telefone"].ToString();
+                            txtRegiao.Text = reader["regiao"].ToString();
+                            txtEmail.Text = reader["email"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar dados do perfil: " + ex.Message);
+            }
         }
 
         private void btnEditar_Click(object sender, RoutedEventArgs e)
